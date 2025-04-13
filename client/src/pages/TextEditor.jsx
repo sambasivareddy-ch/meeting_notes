c945ffe942e9412e8b6a98c27945db5603c9e037
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-04-13 10:22:11 +0530

    Update TextEditor.jsx with Env

--- client/src/pages/TextEditor.jsx
+++ client/src/pages/TextEditor.jsx
@@ -15,7 +15,7 @@ const TextEditor = (props) => {
         const getNotes = async () => {
             try {
                 const response = await fetch(
-                    `http://localhost:8080/meetings/${id}/notes`,
+                    `${import.meta.env.VITE_APP_SERVER_URI}/meetings/${id}/notes`,
                     {
                         method: "GET",
                         headers: {
@@ -40,7 +40,7 @@ const TextEditor = (props) => {
     const saveBtnClickHandler = async () => {
         try {
             const response = await fetch(
-                `http://localhost:8080/meetings/${id}/notes`,
+                `${import.meta.env.VITE_APP_SERVER_URI}/meetings/${id}/notes`,
                 {
                     method: "POST",
                     headers: {
