2cdcc0130aa28ec65f75ab7baf54fd7915840ba5
V N G Samba Siva Reddy Chinta <151616423+sambasivareddy-ch@users.noreply.github.com>
2025-04-14 14:31:18 +0000

    Cookie Set with http.SetCookie instead of Gin

--- server/routes/set_session.go
+++ server/routes/set_session.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,7 +20,17 @@ func SetCookieRoute(ctx *gin.Context) {
 		return
 	}
 
-	ctx.SetCookie("session_id", body.Token, 24*60*60, "/", "meeting-notes-phi.vercel.app", true, true)
+	// ctx.SetCookie("session_id", body.Token, 24*60*60, "/", "meeting-notes-phi.vercel.app", true, true)
+
+	http.SetCookie(ctx.Writer, &http.Cookie{
+		Name: "session_id",
+		Value: body.Token,
+		Path: "/",
+		Expires: time.Now().Add(24 * time.Hour),
+		HttpOnly: true,
+		Secure: true,
+		SameSite: http.SameSiteNoneMode,
+	})
 
 	ctx.JSON(http.StatusOK, gin.H{
 		"status": "session created",
